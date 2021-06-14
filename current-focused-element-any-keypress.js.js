2c2fda5167ec575b61cd5fdd366c115ab0a5c7e6
ernesto-code <60942733+ernesto-code@users.noreply.github.com>
2021-06-14 11:56:26 -0400

    Create current-focused-element-any-keypress.js.js

---
+++ current-focused-element-any-keypress.js.js
@@ -0,0 +1,9 @@
+// This scripts Logs in console which element has 
+
+document.addEventListener("keydown", (event) => {
+            setTimeout(() => {
+                console.clear()
+                console.log("Current Focused Element:")
+                console.log(document.activeElement)
+            }, 55)
+})
