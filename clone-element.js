009f3ddc6e40445d28d2b6463e5947325844cee8
ernesto-code <60942733+ernesto-code@users.noreply.github.com>
2021-06-15 14:33:04 -0400

    Update clone-element.js

--- clone-element.js
+++ clone-element.js
@@ -1,35 +1,41 @@
-//    C L O N E
-/* 
+/*
+INSTRUCTIONS:
 
-This script clones the target element once the user clicks on it. An id must be provided to the target element.
+1-Inspect the code and find the dynamically added element's container.
+2-Add/Modify its class attribute's value, ensuring it contains the "a11ytest" class.
+3-Open the browser console.
+4-Activate the "Clone element" bookmark.
+5-Check that "Observed and Cloned  .... Done!" has been log in console.
+6-The dynamically added element must be already included in the DOM.
 
-Instructions:
-1. Find the element to be cloned using the Elements Inspector on the Dev Tools.
-2. Add an id attribte with the correct value ("mycombo" in this case)
-3. Open the console (Shift + Ctrl + P in Chrome) and paste the script code.
-4. Press enter and verify that "undefined" is printed on the console in case of error refresh the page and start over.
-5. When cloning comboboxes, navigate to the element using keyboard and when the list is displayed, click on the field. 
-6. There must be a message on the console saying that the element was cloned as sibling and it must be visible on the page.
-7. Finally, remove from the DOM the older combobox (target element), otherwise there will be errors of duplicated elements on the page. 
 */
 
 
-// SCRIPT CODE (Combobox)
+const myElement = document.querySelector("#myelement");
+let done = false;
 
-const comboBox = document.querySelector("#mycombo")
+const observer = new MutationObserver(mutations => {
+    if (!done) {
+        console.log(mutations);
 
-const cloneElement = (elementToClone) => {
-    let clonedElement = elementToClone.parentElement.cloneNode(true)
-    elementToClone.parentElement.appendChild(clonedElement)
-    console.log("Element cloned and inserted as sibling!")
-}
-comboBox.addEventListener("click", (event) => {
-    console.log("Event Listener .... added!")
-    setTimeout(() => {
-        cloneElement(comboBox)
-	comboBox.remove()
-    }, 1000)
-})
+        if (mutations[0].addedNodes.length > 0) {
 
+            console.log("Observing....");
+            console.log("Cloning  .... ");
 
+            setTimeout(() => {
+                const addedNode = mutations[0].addedNodes[0].cloneNode(true);
+                myElement.appendChild(addedNode);
+
+                done = true;
+                console.log("Observed and Cloned  .... Done!");
+
+            }, 500);
+        };
+    };
+});
+
+observer.observe(myElement, {
+    childList: true
+});
 
