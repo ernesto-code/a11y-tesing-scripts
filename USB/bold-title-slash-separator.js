8bc14efad12c96d0dc1964b564148f09ca185e55
Ernesto Alonso <60942733+ernesto-code@users.noreply.github.com>
2022-11-28 17:54:02 -0500

    feat: Scripts organization by folder and addition of the new ones

---
+++ USB/bold-title-slash-separator.js
@@ -0,0 +1,22 @@
+// BOLD TITLES in platforms
+var index;
+var first;
+var second;
+let obtenerDato = document.getElementsByTagName("td");
+let title = ''
+let titleDesc =''
+let arrTitle =[]
+
+for (index = 0; index < obtenerDato.length; index++ ){    
+    if(obtenerDato[index].getAttribute('data-column-title')=='Title '){
+    first= obtenerDato[index].childNodes;    
+    second = first[3].childNodes; 
+    title = second[2].innerHTML
+    arrTitle = title.split('/')
+    titleDesc = '<b>'+ arrTitle[arrTitle.length -1]+'</b>'
+    arrTitle.pop()
+    arrTitle.push(titleDesc)
+    second[2].innerHTML = arrTitle.join('/')
+    }
+
+}
---
+++ UTy/tc-preview-format.js
@@ -0,0 +1,4 @@
+// Ensure the preview dialog is displayed before running the script
+
+const info = document.getElementsByClassName('details')
+info[0].style.setProperty('white-space','break-spaces','important')
