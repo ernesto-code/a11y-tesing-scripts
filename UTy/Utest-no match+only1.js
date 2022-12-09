919004980470d549ef95191d43ac492c0cae2196
hermesrodriguezquesada <75341312+hermesrodriguezquesada@users.noreply.github.com>
2022-12-09 09:08:01 +0000

    Add:Create 3 scripts

---
+++ UTy/Utest-no match+only1.js
@@ -0,0 +1,17 @@
+let obtenerDatos = document.getElementsByTagName("span");
+var result;
+var result2=[];
+var mostrar;
+
+for (index = 0; index < obtenerDatos.length; index++ ){
+    result= obtenerDatos[index].innerText;
+    if(obtenerDatos[index].getAttribute('ng-if') =='row.numberOfReproducers > 0' && result == 2 ||
+      obtenerDatos[index].getAttribute('class') =='bug-status status-2' ||
+      obtenerDatos[index].getAttribute('class') =='bug-status status-3' || 
+      obtenerDatos[index].getAttribute('class') =='bug-status status-11' ||
+      obtenerDatos[index].getAttribute('class') =='bug-status status-12') 
+    {
+        mostrar = obtenerDatos[index].parentNode.parentNode;
+        mostrar.setAttribute('style', 'display:none');
+    }
+}
---
+++ UTy/Utest-view components +1.js
@@ -0,0 +1,18 @@
+let obtenerDatos = document.getElementsByTagName("span");
+var result;
+var result2=[];
+var mostrar;
+
+for (index = 0; index < obtenerDatos.length; index++ ){
+    result= obtenerDatos[index].innerText;
+    if((obtenerDatos[index].getAttribute('class') !='bug-status status-11'||
+       obtenerDatos[index].getAttribute('class') !='bug-status status-2' ||
+       obtenerDatos[index].getAttribute('class') !='bug-status status-12'||
+       obtenerDatos[index].getAttribute('class') !='bug-status status-3' || 
+       obtenerDatos[index].getAttribute('class') =='bug-status status-1')  && 
+       (obtenerDatos[index].getAttribute('ng-if') =='row.numberOfReproducers > 0' && (result == '1'))){
+        mostrar = obtenerDatos[index].parentNode.parentNode;
+        mostrar.setAttribute('style', 'display:none');
+    }
+    console.log(mostrar);
+}
