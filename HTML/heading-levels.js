f145d61201aa701a4e81a3925d1e062bea65056a
Ernesto Alonso <60942733+ernesto-code@users.noreply.github.com>
2023-02-24 17:31:55 -0500

    update : adding the visual indicators of headings using a container first round

--- HTML/heading-levels.js
+++ HTML/heading-levels.js
@@ -1,3 +1,32 @@
+// Version 2.0
+/*
+2.0 en esta version la intension es usar un layout para contener todos los identificadores de los
+elementos como el Accessibility Insight ext
+*/
+
+
+const bodyElement = document.querySelector('body')
+
+// Adding container for the identification divs
+const divContainer = document.creat
+// Creating the identification divs according to the HTML element
+// Adding divs to container
+// Insering container in DOM
+
+position: absolute !important;
+    z-index: 2147483646 !important;
+    top: 0 !important;
+    left: 0 !important;
+    overflow: visible !important;
+    pointer-events: none !important;
+    visibility: visible !important;
+
+
+
+
+
+
+
 const applyStyle = (elm, color)=>{
     elm.style.cssText = 'width: fit-content;height: fit-content;padding:10px;border-radius: 5px;background-color: red; color:white;outline: white thin solid;outline-offset: -2px;display:inline;font-size:15px;font-weight:normal;font-family:Arial, Helvetica, sans-serif;text-transform: capitalize;'
 }
