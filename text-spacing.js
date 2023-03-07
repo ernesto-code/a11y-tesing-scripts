0134e3796b2ac7f1244443216d05c151a6e63202
Ernesto Alonso <60942733+ernesto-code@users.noreply.github.com>
2023-03-07 15:06:34 -0500

    feat: Text-spacing added

---
+++ text-spacing.js
@@ -0,0 +1,8 @@
+//version 1.0
+
+document.querySelectorAll("*").forEach((el)=>{
+    el.style.setProperty('word-spacing','0.16em','important')
+    el.style.setProperty('line-height','1.5','important')
+    el.style.setProperty('letter-spacing','0.12em','important')
+
+})
