90d9b95bc9ae0349bdfadbaf25ed7f6c3cb7272f
Ernesto Alonso <60942733+ernesto-code@users.noreply.github.com>
2023-03-24 17:24:29 -0400

    Update: mark double click now is marking/unmarking only the tr

--- mark-row-double-click.js
+++ mark-row-double-click.js
@@ -2,6 +2,18 @@
 
 // Mark Row
 document.addEventListener('dblclick',(ev)=>{
-    console.log(ev.target)
-    ev.target.parentElement.style.background = "#bddfff";
+    const targetElm = ev.target
+    const parentElement = ev.target.parentElement
+    console.log(targetElm)
+    if(targetElm.tagName == 'TD'){
+        console.log(parentElement.style.background)
+        if(parentElement.style.background == "rgb(189, 223, 255)"){
+            console.log('remove background')
+            parentElement.style.background = "none";
+        }
+        else{
+            parentElement.style.background = "rgb(189, 223, 255)";
+            console.log('add background')
+        }    
+    } 
 })
