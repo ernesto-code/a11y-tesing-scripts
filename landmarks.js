b996b77e4a4a71fc21b1438011e86d6681ba2e5c
atamayo <atamayoquintero@gmail.com>
2023-10-04 11:33:22 -0500

    fix: prioritize ARIA roles and exclude native tags with roles

--- landmarks.js
+++ landmarks.js
@@ -1,4 +1,3 @@
-// (() => {
 const landmarksNative = [
   { selector: 'header', ariaEquivalent: 'banner' },
   { selector: 'nav', ariaEquivalent: 'navigation' },
@@ -15,35 +14,38 @@ const landmarkRoles = [
 
 let ariaLandmarkCount = 0
 let nativeLandmarkCount = 0
+let highlightedElements = new Set()
 
-//Find and highlight NATIVE landmarks
-console.log('NATIVE Landmark Roles in use:')
-landmarksNative.forEach((item) => {
-  const elements = document.querySelectorAll(item.selector)
-  elements.forEach((element) => {
-    const firstLabel = highlightElement(element, item.selector, 'red')
-    element.firstLabelWidth = firstLabel.offsetWidth
-    nativeLandmarkCount++
-  })
-  if (elements.length !== 0) {
-    console.log(`${item.selector}: ${elements.length} occurrences`)
-  }
-})
-
-console.log(' ')
 //Find and highlight ARIA landmarks
 console.log('ARIA Landmark Roles in use:')
 landmarkRoles.forEach((role) => {
   const elements = document.querySelectorAll(`[role="${role}"]`)
   const formattedRole = 'a' + role.charAt(0).toUpperCase() + role.slice(1)
   elements.forEach((element) => {
-    const additionalLeft = element.firstLabelWidth || 0
-    highlightElement(element, formattedRole, '#00F', additionalLeft) // store the label element
-
+    highlightElement(element, formattedRole, '#00F') // store the label element
+    highlightedElements.add(element)
     ariaLandmarkCount++
   })
   if (elements.length !== 0) {
     console.log(`${formattedRole}: ${elements.length} occurrences`)
+  }
+})
+
+//Find and highlight NATIVE landmarks
+console.log(' ')
+console.log('NATIVE Landmark Roles in use:')
+landmarksNative.forEach((item) => {
+  const elements = document.querySelectorAll(item.selector)
+  elements.forEach((element) => {
+    if (element.hasAttribute('role')) {
+      highlightedElements.add(element)
+    } else if (!highlightedElements.has(element)) {
+      highlightElement(element, item.selector, 'red')
+      nativeLandmarkCount++
+    }
+  })
+  if (elements.length !== 0) {
+    console.log(`${item.selector}: ${elements.length} occurrences`)
   }
 })
 
@@ -60,7 +62,7 @@ if (ariaLandmarkCount === 0 && nativeLandmarkCount === 0) {
 }
 
 //Highlights a given HTML element and adds a label with a specified text.
-function highlightElement(element, label, color, additionalLeft = '0') {
+function highlightElement(element, label, color) {
   element.style.outline = `2px solid ${color === '#00F' ? color : 'none'}`
   element.style.border = `2px solid ${color === 'red' ? color : 'none'}`
   // element.style.boxShadow  = `0 0 0 2px ${color === '#00F' ? color : 'none'}`
@@ -76,7 +78,7 @@ function highlightElement(element, label, color, additionalLeft = '0') {
   labelElement.style.position = 'absolute'
   labelElement.style.padding = '2px'
   labelElement.style.top = '0'
-  labelElement.style.left = additionalLeft + 'px'
+  labelElement.style.left = '0'
   labelElement.style.zIndex = 999
   labelElement.style.background = color
   labelElement.style.color = 'white'
@@ -84,4 +86,3 @@ function highlightElement(element, label, color, additionalLeft = '0') {
   element.prepend(labelElement)
   return labelElement
 }
-// })()
